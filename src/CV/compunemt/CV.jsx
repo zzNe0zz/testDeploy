13742b97e1638dbaefe9fd7e72862a9c8cdbd14c
zzNe0zz <zzhoangzz1995@gmail.com>
2022-11-08 10:52:20 +0700

    demo-deploy

---
+++ src/CV/compunemt/CV.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Body from './Body'
+import Header from './Header'
+import Footer from './Footer'
+function CV() {
+  return (
+    <div>
+        <Header></Header>
+        <Body></Body>
+        <Footer></Footer>
+    </div>
+  )
+}
+
+export default CV
